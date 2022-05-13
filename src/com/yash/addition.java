0c6a4d1230f2eed74fac3539e7199a6f2982310e
akash.nandurkar <akash.nandurkar@YPH1015340LT.Yash.com>
2022-05-13 20:46:52 +0530

    first commit

---
+++ src/com/yash/addition.java
@@ -0,0 +1,17 @@
+package com.yash;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		int b = 10;
+		
+		int c = a + b;
+		
+		System.out.println(" Addition = "+c);
+
+	}
+
+}
